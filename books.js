baec299757fb45252b9e9c8d42fe98a4c6014a2b
Andy <andybriggs84@gmail.com>
2016-11-07 20:22:06 +0000

    attach list to DOM

--- books.js
+++ books.js
@@ -1,17 +1,30 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=javascript', true);
-
-request.onload = function() {
-  if (request.status >= 200 && request.status < 400) {
-    var data = JSON.parse(request.responseText);
-    console.log(data);
-  } else {
-    console.log('something went wrong');
-  }
+var bookList = {
+  getBooks: function() {
+    var request = new XMLHttpRequest(),
+    _this = this;
+    request.open('GET', 'https://www.googleapis.com/books/v1/volumes?q=javascript&maxResults=20&orderBy=newest', true);
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(request.responseText);
+        _this.createBookList(data.items);
+      } else {
+        console.log('something went wrong');
+      }
+    };
+    request.onerror = function() {
+      console.log('Something went wrong');
+    };
+    request.send();
+  },
+  createBookList: function(books) {
+    var newList = document.createElement('ul');
+    for (var i = 0; i < books.length; i++) {
+      var item = document.createElement('li');
+      item.appendChild(document.createTextNode(books[i].volumeInfo.title));
+      newList.appendChild(item);
+    };
+    document.getElementById('app').appendChild(newList);
+  },
 };
 
-request.onerror = function() {
-  // There was a connection error of some sort
-};
-
-request.send();
+bookList.getBooks();
