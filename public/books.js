09b984d53e4c0ca12b98e913c7098eefe39dbc93
Andy Briggs <andy.briggs@dctfh.com>
2016-11-09 15:27:06 +0000

    added create book method

--- public/books.js
+++ public/books.js
@@ -22,21 +22,15 @@ booksApp.createBookList = function(books){
   var newList = document.createElement('ul');
 
   for (var i = 0; i < books.length; i++) {
-
-    var book = {
-      title: books[i].volumeInfo.title,
-      description: books[i].volumeInfo.description,
-      thm: books[i].volumeInfo.imageLinks ? books[i].volumeInfo.imageLinks.smallThumbnail : './no-image.jpg'
-    };
-
-    var row = this.createBookRow(book.title, book.description, book.thm);
+    var book = this.createBook(books[i]);
+    var row = this.createBookRow(book.title, book.description, book.thm, book.alt);
     newList.appendChild(row);
 
   };
   document.getElementById('app').appendChild(newList);
 };
 
-booksApp.createBookRow = function(title, description, imgSrc) {
+booksApp.createBookRow = function(title, description, imgSrc, alt) {
   var listItem = document.createElement('li'),
   summary = this.createSummary(description, 200);
 
@@ -48,12 +42,22 @@ booksApp.createBookRow = function(title, description, imgSrc) {
   bookTitle.appendChild(document.createTextNode(title));
   bookDscr.appendChild(document.createTextNode(summary));
   bookThm.setAttribute('src', imgSrc);
+  bookThm.setAttribute('alt', alt);
 
   listItem.appendChild(bookContainer);
   bookContainer.appendChild(bookTitle);
   bookContainer.appendChild(bookThm);
   bookContainer.appendChild(bookDscr);
   return listItem;
+};
+
+booksApp.createBook = function(book) {
+  return {
+    title: book.volumeInfo.title,
+    description: book.volumeInfo.description,
+    thm: book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : './no-image.jpg',
+    alt: book.volumeInfo.imageLinks ? book.volumeInfo.title + ' cover' : 'No cover available'
+  }
 };
 
 booksApp.createSummary = function(description, charCount) {
